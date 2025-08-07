21014523c7bfffe9e4a810ae005bf0d634242cc3
DarsHandegithub <163905887+DarsHandegithub@users.noreply.github.com>
2025-08-07 12:58:52 +0530

    Add files via upload

---
+++ jdbc_mini_project/src/com/aurionpro/test/Test.java
@@ -0,0 +1,17 @@
+package com.aurionpro.test;
+
+import com.aurionpro.controller.AdminController;
+
+public class Test {
+
+	public static void main(String[] args) {
+		AdminController adminController = AdminController.getObject();
+		Driver driver = new Driver();
+		
+		if(adminController.login()) {
+		driver.driverMenu();
+		}
+		
+	}
+
+}
